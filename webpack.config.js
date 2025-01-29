0b039612d1baab572a26aa91d19812f6b7383de3
Michal Wisniewski <michalwisniewskifrontend@onet.pl>
2025-01-29 14:35:03 +0100

    react boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.(css)$/,
+                exclude: /node_modules/,
+                use: ['sass-loader','style-loader', 'css-loader']
+            },
+            {
+                test: /\.(sass|scss)$/,
+                // use: ['style-loader', 'css-loader']
+                use: ['css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpg|png|svg|gif|jpeg)$/,
+                use: 'file-loader'
+               
+            },
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    }
+}
